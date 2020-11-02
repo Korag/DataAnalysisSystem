d833f0f2177532f39ac26d0c1e0642dbe8d6b71a
Łukasz Czepielik <koragrex@gmail.com>
2020-11-02 23:23:37 +0100

    Added AspNetCore.Identity.Mongo project.

---
+++ AspNetIdentityProvider/AspNetCore.Identity.Mongo/MongoIdentityOptions.cs
@@ -0,0 +1,15 @@
+﻿namespace AspNetCore.Identity.Mongo
+{
+	public class MongoIdentityOptions
+	{
+		public string ConnectionString { get; set; } = "mongodb://localhost/default";
+        
+	    public string UsersCollection { get; set; } = "Users";
+		
+	    public string RolesCollection { get; set; } = "Roles";
+
+        public string MigrationCollection { get; set; } = "_Migrations";
+
+	    public bool UseDefaultIdentity { get; set; } = true;
+    }
+}
