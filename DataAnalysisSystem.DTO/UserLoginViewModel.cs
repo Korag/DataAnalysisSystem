281418dee1a8d1eea7baf798c0929278bdcf7a57
Łukasz Czepielik <koragrex@gmail.com>
2021-01-11 01:11:46 +0100

    Added UserLogin razor page.

---
+++ DataAnalysisSystem.DTO/UserLoginViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DataAnalysisSystem.DTO
+{
+    public class UserLoginViewModel
+    {
+        [Required(ErrorMessage = "Field \"{0}\" is required.")]
+        [Display(Name = "Email address")]
+        [EmailAddress(ErrorMessage = "Field \"{0}\" does not contain a valid email address.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Field \"{0}\" is required.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember me")]
+        public bool RememberMe { get; set; }
+    }
+}
