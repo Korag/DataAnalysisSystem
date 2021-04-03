0f1c1753404ff79b8468ddf296c48fc4e61df54c
Łukasz Czepielik <koragrex@gmail.com>
2021-04-04 00:33:49 +0200

    Added _DataTableWLoadingAndTabsInitScript partial View with appropriate ViewModel.

---
+++ DataAnalysisSystem.DTO/Scripts/DataTableInitScriptWithLoadingAndTabsViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace DataAnalysisSystem.DTO.Scripts
+{
+    public class DataTableInitScriptWithLoadingAndTabsViewModel
+    {
+        public string DataTableId { get; set; }
+        public string LoadingIndicatorId { get; set; }
+        public IList<string> TabsContentDivsId { get; set; }
+    }
+}
