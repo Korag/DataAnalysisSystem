5ff7026b919072a65378e3b5c573e0b924e38c13
Łukasz Czepielik <koragrex@gmail.com>
2021-04-03 23:38:47 +0200

    Added _UnwrapDataTableSciprt partial View.

---
+++ DataAnalysisSystem.DTO/Scripts/UnwrapDataTableScriptViewModel.cs
@@ -0,0 +1,11 @@
+﻿using DataAnalysisSystem.DTO.DatasetDTO;
+
+namespace DataAnalysisSystem.DTO.Scripts
+{
+    public class UnwrapDataTableScriptViewModel
+    {
+        public DatasetContentViewModel DatasetContent { get; set; }
+        public string ButtonId { get; set; }
+        public string DataTableId { get; set; }
+    }
+}
---
+++ DataAnalysisSystem.DTO/Scripts/UnwrapEditDataTableScriptViewModel.cs
@@ -0,0 +1,11 @@
+﻿using DataAnalysisSystem.DTO.DatasetDTO;
+
+namespace DataAnalysisSystem.DTO.Scripts
+{
+    public class UnwrapEditDataTableScriptViewModel
+    {
+        public EditDatasetContentViewModel DatasetContent { get; set; }
+        public string ButtonId { get; set; }
+        public string DataTableId { get; set; }
+    }
+}
