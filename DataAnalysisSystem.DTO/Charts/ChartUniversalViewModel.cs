d64b208e80a314315faf44a0fbd612ac1083990b
Łukasz Czepielik <koragrex@gmail.com>
2021-04-04 02:12:49 +0200

    Added _AttributeDistributionChart partial View with appropriate ViewModel.

---
+++ DataAnalysisSystem.DTO/Charts/ChartUniversalViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace DataAnalysisSystem.DTO.Charts
+{
+    public class ChartUniversalViewModel
+    {
+        public string ChartCanvasId { get; set; }
+        public string Data { get; set; }
+    }
+}
