534f7915bee230a5d4b1e45f2756c48ea8749232
Łukasz Czepielik <koragrex@gmail.com>
2021-04-14 10:55:15 +0200

    Personalized and added _DataTableOnlyHeaderWLoadingIndicatorInitScript.

---
+++ DataAnalysisSystem.DTO/Scripts/DataTableOnlyHeaderWLoadingIndicatorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DataAnalysisSystem.DTO.Scripts
+{
+    public class DataTableOnlyHeaderWLoadingIndicatorViewModel
+    {
+        public string DataTableId { get; set; }
+        public string LoadingIndicatorId { get; set; }
+        public string EmptyDataAnnotation { get; set; }
+    }
+}
