b996ce4c2010ab3142f0d37d0fcddf91d8831c4c
Łukasz Czepielik <koragrex@gmail.com>
2021-04-15 19:43:04 +0200

    Added Checkboxes validation inside PerformNewAnalysis action View.

---
+++ DataAnalysisSystem.DTO/Scripts/CheckboxLimiterValidationScriptViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DataAnalysisSystem.DTO.Scripts
+{
+    public class CheckboxLimiterValidationScriptViewModel
+    {
+        public string CheckboxClass { get; set; }
+        public string CheckboxContainerId { get; set; }
+        public int MinAmountOfCheckedCheckboxes { get; set; }
+    }
+}
---
+++ DataAnalysisSystem.DTO/Scripts/ValidateFormWithCheckboxesAndOpenModalScriptViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace DataAnalysisSystem.DTO.Scripts
+{
+    public class ValidateFormWithCheckboxesAndOpenModalScriptViewModel
+    {
+        public ValidateFormWithCheckboxesAndOpenModalScriptViewModel()
+        {
+            this.CheckboxesToLimitValidation = new List<CheckboxLimiterValidationScriptViewModel>();
+        }
+
+        public string FormId { get; set; }
+        public string ModalId { get; set; }
+        public string ValidationContainerId { get; set; }
+        public IList<CheckboxLimiterValidationScriptViewModel> CheckboxesToLimitValidation { get; set; }
+    }
+}
